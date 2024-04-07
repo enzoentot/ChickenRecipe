71afe02db8558c1033248e8c911f6af7c7731422
Enzo <Enzo@ENZO>
2024-04-06 19:11:45 -0700

    Add project files.

---
+++ ChickenRecipe/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.ComponentModel.Design;
+using Recipe;
+
+namespace ChickenRecipe
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HAPPY HOLODAYS! ");
+            Console.WriteLine("1.JANUARY");
+            Console.WriteLine("2.FEBUARY");
+            Console.WriteLine("3.MARCH");
+            Console.WriteLine("4.APRIL");
+            Console.WriteLine("5.MAY");
+            Console.WriteLine("6.JUNE");
+            Console.WriteLine("7.JULY");
+            Console.WriteLine("8.AUGUST");
+            Console.WriteLine("9.SEPTEMBER");
+            Console.WriteLine("10.OCTOBER");
+            Console.WriteLine("11.NOVEMBER");
+            Console.WriteLine("12.DECEMBER");
+
+
+            string instructions = Console.ReadLine();
+
+            if (instructions == "1")
+            {
+                Months.January();
+            }
+            else if (instructions == "2")
+            {
+                Months.Febuary();
+            }
+            else if (instructions == "3")
+            {
+                Months.March();
+            }
+
+            else if (instructions == "4")
+            {
+                Months.April();
+            }
+
+            else if (instructions == "5")
+            {
+                Months.May();
+            }
+
+            else if (instructions == "6")
+            {
+                Months.June();
+            }
+
+            else if (instructions == "7")
+            {
+                Months.July();
+            }
+
+            else if (instructions == "8")
+            {
+                Months.August();
+            }
+
+            else if (instructions == "9")
+            {
+                Months.September();
+            }
+
+            else if (instructions == "10")
+            {
+                Months.October();
+            }
+
+            else if (instructions == "11")
+            {
+                Months.November();
+            }
+
+            else if (instructions == "12")
+            {
+                Months.December();
+            }
+
+            else
+            {
+
+                Console.WriteLine("Ivalid.Please RE-enter a valid MONTH");
+            }
+        }
+            
+        }
+    }
+
+
+
+
+        
+    
+
---
+++ Recipe/Chicken.cs
@@ -0,0 +1,125 @@
+﻿using System;
+
+namespace Recipe
+{
+    public class Months
+    {
+        public static void January()
+        {
+            Console.WriteLine(" ");
+            Console.WriteLine("JANUARY: ");
+            Console.WriteLine("01st - NEW YEARS EVE");
+            Console.WriteLine("06th - EPIPHANY");
+            Console.WriteLine("09th - BLACK NAZARENE");
+            Console.WriteLine("16th - FEAST STO NINO");
+            Console.WriteLine("25TH - BURNS NIGHT");
+
+        }
+        public static void Febuary() {
+            Console.WriteLine(" ");
+            Console.WriteLine("FEBUARY: ");
+            Console.WriteLine("01st - CHINESE NEW YEAR");
+            Console.WriteLine("14th - VALENTINES");
+            Console.WriteLine("25th - EDSA REVOLUTION");
+
+        }
+
+        public static void March() {
+            Console.WriteLine("");
+            Console.WriteLine("MARCH: ");
+            Console.WriteLine("25th - PEOPLE POWER ANNIVERSARY");
+            Console.WriteLine("28th - MAUNDY THURSDAY");
+            Console.WriteLine("29th - GOOD FRIDAY");
+            Console.WriteLine("30th - BLACK SATURDAY");
+
+        }
+        public static void April() {
+            Console.WriteLine(" ");
+            Console.WriteLine("APRIL: ");
+            Console.WriteLine("01st - APRIL FOOLS");
+            Console.WriteLine("21st - EASTER SUNDAY");
+            Console.WriteLine("22nd - EARTH DAY");
+            
+        }
+        
+        public static void May()
+        {
+            Console.WriteLine(" ");
+            Console.WriteLine("MAY: ");
+            Console.WriteLine("01st - LABOR DAY");
+            Console.WriteLine("12th - MOTHERS DAY");
+            Console.WriteLine("27TH - MEMORIAL DAY");
+
+        }
+
+        public static void June()
+        {
+            Console.WriteLine(" ");
+            Console.WriteLine("JUNE: ");
+            Console.WriteLine("05th - WORLD ENVI DAY");
+            Console.WriteLine("12th - INDEPENDENCE DAY");
+            Console.WriteLine("16th - FATHERS DAY");
+            Console.WriteLine("19th - JUNETEENTH");
+        }
+
+        public static void July()
+        {
+            Console.WriteLine(" ");
+            Console.WriteLine("JULY: ");
+            Console.WriteLine("04th - INDEPENDENCE DAY");
+            Console.WriteLine("14th - BASTILLE DAY");
+            Console.WriteLine("31st - EID AL AHDA");
+        }
+
+        public static void August()
+        {
+            Console.WriteLine(" ");
+            Console.WriteLine("AUGUST:" ); 
+            Console.WriteLine("5th - INTERNATIONAL FRIENDSHIP DAY");
+            Console.WriteLine("9th - NATIONAL DAY");
+            Console.WriteLine("21st - NINOY AQUINO DAY");
+        }
+
+        public static void September()
+        {
+            Console.WriteLine(" ");
+            Console.WriteLine("SEPTEMBER: ");
+            Console.WriteLine("11th - AUTUMN FESTIVAL");
+            Console.WriteLine("08 - MAMA MARY DAY");
+            Console.WriteLine("10th - TEACHERS DAY ");
+            Console.WriteLine("26th - NATIONAL HEROES DAY");
+
+        }
+
+        public static void October()
+        {
+            Console.WriteLine(" ");
+            Console.WriteLine("OCTOBER: ");
+            Console.WriteLine("3rd - UNITY DAY");
+            Console.WriteLine("5th - WOLDS TEACHER DAY");
+            Console.WriteLine("14th - THANKSGIVING");
+            Console.WriteLine("31st - HOLLOWEEN");
+        }
+
+        public static void November()
+        {
+            Console.WriteLine(" ");
+            Console.WriteLine("NOVEMBER: ");
+            Console.WriteLine("01st - ALL SAINTS DAY ");
+            Console.WriteLine("2nd - ALL SOULS DAY");
+            Console.WriteLine("30th - BONIFACIO DAY");
+        }
+        
+        public static void December()
+        {
+            Console.WriteLine(" ");
+            Console.WriteLine("DECEMBER: ");
+            Console.WriteLine("2nd - HANNUKAH ");
+            Console.WriteLine("25th - CHRISTMAS EVE ");
+            Console.WriteLine("30th - RIZAL DAY ");
+            Console.WriteLine("31st - NEW YEARS EVE");
+
+        }
+
+    }
+}
